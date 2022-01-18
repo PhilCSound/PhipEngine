68bac95aadcd795f98d9314452af60982fb1cf72
Phillip Cook <yru2kind@gmail.com>
2022-01-18 16:41:04 -0600

    Added more operator overloads and ToString method.

--- PhipEngine/Vec2F.cpp
+++ PhipEngine/Vec2F.cpp
@@ -12,12 +12,55 @@ Vec2F Vec2F::operator+(const Vec2F & _RHS)
 	return Vec2F(X + _RHS.X, Y + _RHS.Y);
 }
 
+Vec2F & Vec2F::operator+=(const Vec2F & _RHS)
+{
+	X += _RHS.X;
+	Y += _RHS.Y;
+	return *this;
+}
+
 Vec2F Vec2F::operator-(const Vec2F & _RHS)
 {
 	return Vec2F(X - _RHS.X, Y - _RHS.Y);
 }
 
+Vec2F & Vec2F::operator-=(const Vec2F & _RHS)
+{
+	X -= _RHS.X;
+	Y -= _RHS.Y;
+	return *this;
+}
+
+Vec2F Vec2F::operator*(int _RHS)
+{
+	return Vec2F(X * _RHS, Y * _RHS);
+}
+
+Vec2F Vec2F::operator*(float _RHS)
+{
+	return Vec2F(X * _RHS, Y * _RHS);
+}
+
+Vec2F & Vec2F::operator*=(int _RHS)
+{
+	X *= _RHS;
+	Y *= _RHS;
+	return *this;
+}
+
+Vec2F & Vec2F::operator*=(float _RHS)
+{
+	X *= _RHS;
+	Y *= _RHS;
+	return *this;
+}
+
 bool Vec2F::operator==(const Vec2F & _RHS)
 {
 	return ((X == _RHS.X) && (Y = _RHS.Y));
+}
+
+const std::string Vec2F::ToString()
+{
+	return "X: " + std::to_string(X) + ". Y: " + std::to_string(Y) + ".";
 }
--- PhipEngine/Vec2F.h
+++ PhipEngine/Vec2F.h
@@ -3,8 +3,8 @@
 #include "SFML/System.hpp"
 
 
-//TODO: Templates. For now I am implementing as a Vec2 Float
-//TODO: Finish writing this.
+//TODO: Templates? For now I am implementing as a Vec2 Float
+//TODO: Add more useful functions.
 class Vec2F
 {
 
@@ -12,13 +12,18 @@ public:
 	//Member Functions
 	Vec2F(float _x = 0, float _y = 0); //Constructor
 	
-	//sf::Vector2f ToSFMLVec2f();
-	//const std::string ToString();
 	Vec2F operator +(const Vec2F& _RHS);
+	Vec2F& operator +=(const Vec2F& _RHS);
 	Vec2F operator -(const Vec2F& _RHS);
+	Vec2F& operator -=(const Vec2F& _RHS);
+	Vec2F operator *(int _RHS);
+	Vec2F operator *(float _RHS);
+	Vec2F& operator *=(int _RHS);
+	Vec2F& operator *=(float _RHS);
 	bool operator ==(const Vec2F& _RHS);
-	
-	
+	const std::string ToString();
+
+
 	//Member Variables
 	float X;
 	float Y;
