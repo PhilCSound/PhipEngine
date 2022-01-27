40eb9eb3882df7e8a604ca38ee50ec8edd34868c
Phillip Cook <yru2kind@gmail.com>
2022-01-26 19:16:52 -0600

    Implemented audio/font loading

--- PhipEngine/ResourceManager.cpp
+++ PhipEngine/ResourceManager.cpp
@@ -1,5 +1,81 @@
 #include "ResourceManager.h"
 
+
+bool ResourceManager::ContainsFont(const std::string & _name)
+{
+	if (fontMap.count(_name) == 0)
+		return false;
+	return true;
+}
+
+bool ResourceManager::ContainsSoundBuffer(const std::string & _name)
+{
+	if (soundBufferMap.count(_name) == 0)
+		return false;
+	return true;
+}
+
+bool ResourceManager::ContainsTexture(const std::string & _name)
+{
+	if (textureMap.count(_name) == 0)
+		return false;
+	return true;
+}
+
+sf::Font & ResourceManager::GetFont(const std::string & _name)
+{
+	if (ContainsFont(_name))
+		return fontMap.at(_name);
+	sf::Font _font;
+	return _font;
+}
+
+sf::SoundBuffer & ResourceManager::GetSoundBuffer(const std::string & _name)
+{
+	if (ContainsSoundBuffer(_name))
+		return soundBufferMap.at(_name);
+	sf::SoundBuffer _sb;
+	return _sb;
+}
+
+sf::Texture & ResourceManager::GetTexture(const std::string & _name)
+{
+	if (ContainsTexture(_name))
+		return textureMap.at(_name);
+	sf::Texture _text;
+	return _text;
+}
+
+bool ResourceManager::LoadFont(const std::string & _path, const std::string & _name)
+{
+	//TODO: Double check if this unneccessary as if a pair already exists nothing happens?
+	if (ContainsFont(_name))
+		return true;
+
+	sf::Font _font;
+	if (_font.loadFromFile(_path))
+	{
+		fontMap.insert(std::make_pair(_name, _font));
+		return true;
+	}
+	return false;
+}
+
+bool ResourceManager::LoadSoundBuffer(const std::string & _path, const std::string & _name)
+{
+	//TODO: Double check if this unneccessary as if a pair already exists nothing happens?
+	if (ContainsSoundBuffer(_name))
+		return true;
+
+	sf::SoundBuffer _sb;
+	if (_sb.loadFromFile(_path))
+	{
+		fontMap.insert(std::make_pair(_name, _sb));
+		return true;
+	}
+	return false;
+}
+
 bool ResourceManager::LoadTexture(const std::string & _path, const std::string & _name)
 {
 	//TODO: Double check if this unneccessary as if a pair already exists nothing happens?
@@ -16,22 +92,19 @@ bool ResourceManager::LoadTexture(const std::string & _path, const std::string &
 }
 
 //Careful: This class/function doesnt know if anything else depends on the file.
+void ResourceManager::UnloadFont(const std::string & _name)
+{
+	fontMap.erase(_name);
+}
+
+//Careful: This class/function doesnt know if anything else depends on the file.
+void ResourceManager::UnloadSoundBuffer(const std::string & _name)
+{
+	soundBufferMap.erase(_name);
+}
+
+//Careful: This class/function doesnt know if anything else depends on the file.
 void ResourceManager::UnloadTexture(const std::string & _name)
 {
 	textureMap.erase(_name);
-}
-
-bool ResourceManager::ContainsTexture(const std::string & _name)
-{
-	if (textureMap.count(_name) == 0)
-		return false;
-	return true;
-}
-
-sf::Texture & ResourceManager::GetTexture(const std::string & _name)
-{
-	if (ContainsTexture(_name))
-		return textureMap.at(_name);
-	sf::Texture _text;
-	return _text;
 }
--- PhipEngine/ResourceManager.h
+++ PhipEngine/ResourceManager.h
@@ -10,12 +10,24 @@ public:
 	ResourceManager(){}
 	~ResourceManager(){}
 
-	bool LoadTexture(const std::string& _path, const std::string& _name);
-	void UnloadTexture(const std::string& _name);
+	//Member functions
+	bool ContainsFont(const std::string& _name);
+	bool ContainsSoundBuffer(const std::string& _name);
 	bool ContainsTexture(const std::string& _name);
+	sf::Font& GetFont(const std::string& _name);
+	sf::SoundBuffer& GetSoundBuffer(const std::string& _name);
 	sf::Texture& GetTexture(const std::string& _name);
-	
+	bool LoadFont(const std::string& _path, const std::string& _name);
+	bool LoadSoundBuffer(const std::string& _path, const std::string& _name);
+	bool LoadTexture(const std::string& _path, const std::string& _name);
+	void UnloadFont(const std::string& _name);
+	void UnloadSoundBuffer(const std::string& _name);
+	void UnloadTexture(const std::string& _name);
+
 private:
+	//Member Variables
+	std::map<std::string, sf::Font> fontMap;
+	std::map<std::string, sf::SoundBuffer> soundBufferMap;
 	std::map<std::string, sf::Texture> textureMap;
 
 };
